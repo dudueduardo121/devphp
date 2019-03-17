2113f20686f64676d0667e2433bef5d21a98f804
Eduardo Gomes <dudueduardo121@gmail.com>
2019-03-17 17:14:16 -0300

    Funções manipulação de arrays

--- index.php
+++ index.php
@@ -1,27 +1,57 @@
-<html>
-<head>
-<meta charset="utf-8">
-<title>index</title>
-<link rel="stylesheet" type="text/css" href="style.css">
-</head>
+<?php
+//Funções
+/*
+function somarNumero($x,$y){
+	return $x + $y;
+}
+$total = somarNumero(10,20);
 
-<body>
- 
- <form method="POST" action= "receber.php" id="formulario" >
- <h1>Ficha de Cadastro para emprego.</h1>
- 	Nome:<input type="text" name="nome"><br><br>
- 	Email:<input type="email" name="email"><br><br>
- 	Data de Nasc.<input type="date" name="data"><br><br>
- 	Cargo:<select name="cargo">
- 		<option>Ajudante</option>
-	    <option>Conferente</option>  
-	    <option>Gerente</option>  
- 	      </select><br><br>
- 	      <input type="submit" name="Enviar">
- </form>
- 
-</body>
-</html>
+echo "A soma e Igual á: ".$total;
+
+
+$x = date('d/m/Y  H:i:s',strtotime("+10 years",0));
+echo $x;
+
+// sorteio com função ran
+$chaveA = array("Paulo", "Roberto", "Antonio", "Lucas");
+$chaveB = array("Carlos", "João", "Marcio", "José");
+
+$a = rand(0, 3);
+$b = rand (0, 3);
+
+echo "O adversário de ".$chaveA[$a]. " será o particpante ".$chaveB[$b]
+
+
+// trocar string por outra
+
+$testo = "o rato rolou";
+$string = str_replace("rato", "gato", $testo);
+
+echo $string;
+
+// caixa baixa e alta
+
+echo strtolower("eduardo");
+echo strtoupper("eduardo");
+
+
+// Manipulação de arrays
+
+$corredores = array("edu","cadu","bidu");
+
+echo "Total do corredores :".count($corredores)."</br></br>";
+$vencedor = rand(0,2);
+
+if(in_array("edu",$corredores)){
+	echo "Vencedor foi ".$corredores[$vencedor]." Parabéns!!!!";
+}
+
+*/
+
+
+
+
+?>
 
 
 
