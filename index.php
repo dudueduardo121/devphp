4731ebd35caba18c6fe7bd860a57dec8e2596355
Eduardo Gomes <dudueduardo121@gmail.com>
2019-03-17 14:24:46 -0300

    Projeto anti F5

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 
 <?php
-// Mutiplos arquivos em php require ou include para mesma pagina tirando o action ="receber.php"
-require "receber.php";
+// Projeto ant f5
+//se o campo foi enviado
+if(isset($_POST['email'])){
+
+	$email = $_POST['email'];
+	// cria um arquivo dentro dele 
+	file_put_contents("text.txt",$email, FILE_APPEND);
+	//redireciona para mesma pagina apos o f5 clicado
+	header("Location: index.php");
+}
 
 ?>
+<link REL="SHORTCUT ICON" HREF="/favicon.ico">
+<form method="POST">
+	<input type="text" name="email"><br>
 
-<form method="POST" action="receber.php">
-	Emai:</br>
-	<input type="text" name="email"/><br>
+	<input type="submit" value="Enviar">
 
-	<input type="submit" value="Enviar dados"/>
 </form>
