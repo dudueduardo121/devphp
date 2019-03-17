fb5712eb91bbde266263355666f2bdaa2d49ace7
Eduardo Gomes <dudueduardo121@gmail.com>
2019-03-17 17:24:38 -0300

    Introdução criptografia php

--- index.php
+++ index.php
@@ -1,54 +1,14 @@
 <?php
-//Funções
-/*
-function somarNumero($x,$y){
-	return $x + $y;
-}
-$total = somarNumero(10,20);
 
-echo "A soma e Igual á: ".$total;
+// introdução a criptografia
 
+$nome = "edu";
+$nome2 = md5($nome);
+$nome3 = base64_encode($nome);
 
-$x = date('d/m/Y  H:i:s',strtotime("+10 years",0));
-echo $x;
-
-// sorteio com função ran
-$chaveA = array("Paulo", "Roberto", "Antonio", "Lucas");
-$chaveB = array("Carlos", "João", "Marcio", "José");
-
-$a = rand(0, 3);
-$b = rand (0, 3);
-
-echo "O adversário de ".$chaveA[$a]. " será o particpante ".$chaveB[$b]
-
-
-// trocar string por outra
-
-$testo = "o rato rolou";
-$string = str_replace("rato", "gato", $testo);
-
-echo $string;
-
-// caixa baixa e alta
-
-echo strtolower("eduardo");
-echo strtoupper("eduardo");
-
-
-// Manipulação de arrays
-
-$corredores = array("edu","cadu","bidu");
-
-echo "Total do corredores :".count($corredores)."</br></br>";
-$vencedor = rand(0,2);
-
-if(in_array("edu",$corredores)){
-	echo "Vencedor foi ".$corredores[$vencedor]." Parabéns!!!!";
-}
-
-*/
-
-
+echo "nome original: ".$nome. "</br>";
+echo "nome criptografado inrevercivel: ".$nome2. "</br>";
+echo "nome criptografado: ".$nome3;
 
 
 ?>
