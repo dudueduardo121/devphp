29c627ca18e3890672878142871fe237fb95f5a3
Eduardo Gomes <dudueduardo121@gmail.com>
2019-03-17 13:38:16 -0300

    incluir na mesma pagina index.php com require ou include

--- index.php
+++ index.php
@@ -1,19 +1,13 @@
 
 <?php
-// switch case php
-$x = 0;  
-switch ($x) {
-	case 0:
-		echo "O x e zero";
-		break;
-	case 1;
-		echo "O x e 1";
-		break;
-	case 2;
-		echo "O x e 2";	
-	default:
-		echo "O x e NULL";
-		break;
-}
+// Mutiplos arquivos em php require ou include para mesma pagina tirando o action ="receber.php"
+require "receber.php";
 
 ?>
+
+<form method="POST" action="receber.php">
+	Emai:</br>
+	<input type="text" name="email"/><br>
+
+	<input type="submit" value="Enviar dados"/>
+</form>
