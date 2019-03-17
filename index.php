668774e0cf5caaeb5e2b42768666792413c8272c
Eduardo Gomes <dudueduardo121@gmail.com>
2019-03-17 13:24:30 -0300

    Switch case em  php

--- index.php
+++ index.php
@@ -1,27 +1,19 @@
 
 <?php
-// verificção senha formulario php  
-if(isset($_GET['email']) && !empty($__GET['email']))
-{
-	If(isset($__GET['senha']) && !empty($__GET['senha']))
-	{
+// switch case php
+$x = 0;  
+switch ($x) {
+	case 0:
+		echo "O x e zero";
+		break;
+	case 1;
+		echo "O x e 1";
+		break;
+	case 2;
+		echo "O x e 2";	
+	default:
+		echo "O x e NULL";
+		break;
+}
 
-		
-		$email = $__GET['email'];
-		$senha = $__GET['senha'];
-
-		echo "Meu email é:".$email."</b> é minha senha e: ".$senha;
-	}	
-} 
 ?>
-
-<div class="container" style="margin-left: 250px; font-family: sans-serif; width: 200px;height: 200px; background-color: tomato; border-radius: 10px;">
-<form method="GET" style="padding: 10px;">
-EMAIL:</br>
-	<input type="text" name="email"><br><br>
-SENHA:</br>
-	<input type="password" name="senha"></br></br>
-	<input type="submit" value="ENVIAR DADOS">
-
-</form>
-</div>
