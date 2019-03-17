22d3286d914db7a35b77055f8be9580780c0613f
Eduardo Gomes <dudueduardo121@gmail.com>
2019-03-16 21:41:54 -0300

    Variaveis constantes

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
+
 <?php
-//Variaveis constantes
+// verificção senha formulario php
+if(isset($_GET['email']) && !empty($__GET['email']))
+{
+	If(isset($__GET['senha']) && !empty($__GET['senha']))
+	{
 
-define("URL","http://www.lbca.com.br");
+		
+		$email = $__GET['email'];
+		$senha = $__GET['senha'];
 
-
-echo "Meu site é:".URL;
-
-
-
-
-
-
+		echo "Meu email é:".$email."</b> é minha senha e: ".$senha;
+	}	
+} 
 ?>
+
+<form method="GET">
+EMAIL:</br>
+	<input type="text" name="email"><br><br>
+SENHA:</br>
+	<input type="password" name="senha"></br></br>
+	<input type="submit" value="ENVIAR DADOS">
+
+</form>
