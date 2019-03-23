3e45a8d5293bd0e8657d7e5c090a9f712278b198
Eduardo Gomes <dudueduardo121@gmail.com>
2019-03-23 19:57:59 -0300

    consulta banco via pdo my sql

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
+/*
 // coneção com o banco de dados
+$mysqli_connection = new MySQLi('localhost','root','','cadastro');
+
+if($mysqli_connection->connect_error){
+	echo "Desconectado! Erro:" .$mysqli_connection->connect_error;
+}else{
+	echo "Conectado!";
+}
+*/
+
+ //CONEXÃO VIA PDO.
 
 $dsn = "mysql:dbname=cadastro;host=localhost";
 $dbuser = "root";
@@ -7,7 +18,23 @@ $dbpass = "";
 
 try{
 	$pdo = new PDO($dsn,$dbuser,$dbpass);
-	echo "conectado...";
+	
+	$sql = "SELECT * FROM usuarios";
+	$sql = $pdo->query($sql);
+
+	if($sql->rowCount()>0){ //rowCount cont as linhas tem que ser maior que 0
+
+		
+		foreach ($sql->fetchAll() as $usuario) { // fetchAll vai pegar todos os usuarios e trnsforma em uma Array
+
+			echo "Nome:".$usuario["nome_usuario"]." - ".$usuario["senha"]."<br>";
+		}
+	}else{
+
+		echo "NÃO HÀ USUARIOS CADASTRADOS";
+	}
+
+
 
 }catch(PDOException $erro){
 	echo "Falha: ".$erro->getMessage();
