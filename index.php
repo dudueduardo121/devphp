716976a780b9949ec9dac57f3c0929ee264f7849
Eduardo Gomes <dudueduardo121@gmail.com>
2019-03-23 20:24:37 -0300

    Update na tabela usuarios

--- index.php
+++ index.php
@@ -18,17 +18,14 @@ $dbpass = "";
 
 try{
 	$pdo = new PDO($dsn,$dbuser,$dbpass);
-// inserindo dados na tabela 
-	$id = "012";
-	$nome_usuario ="Fabiola";
-	$senha = md5("4321");
+// update na tabela do banco
+	$new_Senha = md5("1");
 
-	
-	$sql = "INSERT INTO usuarios SET id = '$id',nome_usuario='$nome_usuario',senha='$senha' ";
+	$sql = "UPDATE usuarios SET senha = '$new_Senha' WHERE id = '1211' ";
+	$pdo->query($sql);
 
-	$sql = $pdo->query($sql);
-	//pega o ultimo id inserido lastInsertId
-	echo "usuario inserido:".$pdo->lastInsertId();
+	echo "usuario atualizado";
+
 
 
 }catch(PDOException $erro){
