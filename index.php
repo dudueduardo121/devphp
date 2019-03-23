4af0d80f1d5271fc5bc9d9f7f5f6fb06bcba7e82
Eduardo Gomes <dudueduardo121@gmail.com>
2019-03-23 20:13:09 -0300

    Inserindo dados na tabela

--- index.php
+++ index.php
@@ -18,22 +18,17 @@ $dbpass = "";
 
 try{
 	$pdo = new PDO($dsn,$dbuser,$dbpass);
+// inserindo dados na tabela 
+	$id = "012";
+	$nome_usuario ="Fabiola";
+	$senha = md5("4321");
+
 	
-	$sql = "SELECT * FROM usuarios";
+	$sql = "INSERT INTO usuarios SET id = '$id',nome_usuario='$nome_usuario',senha='$senha' ";
+
 	$sql = $pdo->query($sql);
-
-	if($sql->rowCount()>0){ //rowCount cont as linhas tem que ser maior que 0
-
-		
-		foreach ($sql->fetchAll() as $usuario) { // fetchAll vai pegar todos os usuarios e trnsforma em uma Array
-
-			echo "Nome:".$usuario["nome_usuario"]." - ".$usuario["senha"]."<br>";
-		}
-	}else{
-
-		echo "NÃO HÀ USUARIOS CADASTRADOS";
-	}
-
+	//pega o ultimo id inserido lastInsertId
+	echo "usuario inserido:".$pdo->lastInsertId();
 
 
 }catch(PDOException $erro){
