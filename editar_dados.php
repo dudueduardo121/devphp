076d44b9b5cffc7156c61efc6a3e8586425be14e
Eduardo Gomes <dudueduardo121@gmail.com>
2019-03-24 11:14:51 -0300

    Ateração alterar dados

--- editar_dados.php
+++ editar_dados.php
@@ -2,28 +2,33 @@
 require 'conexao_banco.php';
 
 $id = 0;
-
-if (isset($_GET['id']) && empty($_GET['id']) ==false) {
+if(isset($_GET['id']) && empty($_GET['id']) == false) {
 	$id = addslashes($_GET['id']);
+}
 
-	$sql = "SELECT * FROM usuarios WHERE id ='$id'";
-	$sql = $pdo->query($sql);
+if(isset($_POST['nome']) && empty($_POST['nome']) == false) {
+	$nome = addslashes($_POST['nome']);
+	$senha =addslashes($_POST['senha']);
 
-	if ($sql->rowCount() > 0) {
-		$dado = $sql->fetch();
-	}else{
-		header("Location:index.php");
-	}
+	$sql = "UPDATE usuarios SET nome_usuario = '$nome', senha = '$senha' WHERE id ='$id'";
+	$pdo->query($sql);
 
 	header("Location: index.php");
+}
+
+$sql = "SELECT * FROM usuarios WHERE id = '$id'";
+$sql = $pdo->query($sql);
+if($sql->rowCount() > 0) {
+	$dado = $sql->fetch();
+} else {
+	header("Location: index.php");
+}
 ?>
 <form method="POST">
-	Id:<br/>
-	<input type="text" name="id"  /><br/>
 	Nome:<br/>
-	<input type="text" name="nome" value="<?php echo $dado['nome_usuario'];?>"/><br/>
+	<input type="text" name="nome" /><br/>
 	Senha:<br/>
 	<input type="password" name="senha"/><br/>
 
-	<input type="submit" value="Atualizar"/>
+	<input type="submit" value="cadastrar"/>
 </form>
--- incluir_novo.php
+++ incluir_novo.php
@@ -3,18 +3,16 @@ require 'conexao_banco.php';
 
 if (isset($_POST['nome']) && empty($_POST['nome'])== false) {
 	$nome = addslashes($_POST['nome']);
-	$email = addslashes($_POST['email']);
 	$senha = md5(addslashes($_POST['senha']));
 
-	$sql = "INSERT INTO usuarios SET id = '$id',nome_usuario ='$nome', senha = '$senha'";
+	$sql = "INSERT INTO usuarios SET nome_usuario ='$nome',senha = '$senha'";
 	$pdo->query($sql);
 
 	header("Location: index.php");
 }
 ?>
 <form method="POST">
-	Id:<br/>
-	<input type="text" name="id"/><br/>
+	
 	Nome:<br/>
 	<input type="text" name="nome"/><br/>
 	Senha:<br/>
