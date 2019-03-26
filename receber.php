73f316fd6cca29de99a1610980ca7951bfa1fdd3
Eduardo Gomes <dudueduardo121@gmail.com>
2019-03-26 20:12:15 -0300

    Função enviar arquivo php receber.php

--- receber.php
+++ receber.php
@@ -1,10 +1,10 @@
-<?php
-extract($_POST);
-
- $nome = array("nome" =>$nome, "email" =>$email, "data" =>$data, "cargo" =>$cargo);
-
-foreach($nome as $candidato) {
-	echo $candidato. "<br>";
-} 
-
+<?php 
+	$arq = $_FILES['arquivo'];
+	//verifica se houve um arquivo que foi enviado
+	if(isset($arq['tmp_name']) && !empty($arq['tmp_name'])) {
+		//função para salvar imagens com cod diferentes
+		$nomearquivo = md5(time().rand(0,99)).'.png';
+		move_uploaded_file($arq['tmp_name'], 'arquivos/'.$nomearquivo);
+		echo "Arquivo enviado com sucesso";
+	}
 ?>
